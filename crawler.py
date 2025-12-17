9f24bea1a8dc5458dc913c9d3163673391b1c3b6
Sibo Huang <zanax10101@gmail.com>
2025-12-17 13:28:59 +0800

    refactor: improve search functionality to return all matching files

--- crawler.py
+++ crawler.py
@@ -1,49 +1,40 @@
 # A file crawler using os.scandir
+# Currently it's very basic and find one file with the keyword in the directory.
+# Potential TODOS
+"""
+- [x] Find all files with the keyword in the directory
+- [ ] Implement SQ Lite database to store the search results
+- [ ] Improve the search algorithm to find the keyword in the file
+- [ ] Improve the terminal CLI interface
+"""
 
 import os
 
-path = "." # Current directory
-target_name = "crawler" # Keyword to search for
-
 def search_directory(path, target_name):
-    '''
-    Use os.scandir to scan entire directory base on the path. 
+    """
+    Use os.scandir to scan entire directory base on the path.
 
     Args:
         path (string): the file path that user sets
         target_name (string): the name of the file that user wants to search for
 
     Returns:
-        string: the path of the file that user wants to search for
-    '''
-    print(f"Scanning directory: {path} for the keyword: {target_name}")
-
+        none
+    """
+    matched_files = []
     with os.scandir(path) as entries:
         for entry in entries:
             if entry.is_file():
                 if target_name.lower() in entry.name.lower():
-                    print(f"Found the file with the keyword: {target_name} \
-                    in {entry.name} at {entry.path}\n")
-                    return entry.path
+                    matched_files.append([entry.name, entry.path])
             elif entry.is_dir():
-                try:
-                    search_directory(entry.path, target_name)
-                except PermissionError:
-                    print(f"Permission denied: {entry.path}")
-                except FileNotFoundError:
-                    print(f"File not found: {entry.path}")
-                except NotADirectoryError:
-                    print(f"Not a directory: {entry.path}")
-                except Exception as e:
-                    print(f"An error occurred: {e}")
-        return None
+                # This fixes the issue with recursive search
+                child_matched_files = search_directory(entry.path, target_name)
+                matched_files.extend(child_matched_files)
+    return matched_files
 
 if __name__ == "__main__":
-    path = input("Enter the path to search for the keyword: ")
-    target_name = input("Enter the keyword to search for: ")
+    path = input("Enter the path to search for the keyword: ").lower()
+    target_name = input("Enter the keyword to search for: ").lower()
     result = search_directory(path, target_name)
-    if result:
-        print(f"Found the file with the keyword: {target_name} \
-        in {result} at {result}\n")
-    else:
-        print(f"No file found with the keyword: {target_name}")
+    print(f"Found {len(result)} matching file(s).")
