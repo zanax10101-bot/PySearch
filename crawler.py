c4bc4e61dbef46db3f6be288ab38a1dc9c9c8a05
Sibo Huang <zanax10101@gmail.com>
2025-12-17 13:53:03 +0800

    refactor: improve error handling and improve output for file search results

--- crawler.py
+++ crawler.py
@@ -1,12 +1,9 @@
 # A file crawler using os.scandir
-# Currently it's very basic and find one file with the keyword in the directory.
-# Potential TODOS
-"""
-- [x] Find all files with the keyword in the directory
-- [ ] Implement SQ Lite database to store the search results
-- [ ] Improve the search algorithm to find the keyword in the file
-- [ ] Improve the terminal CLI interface
-"""
+# Finds files in the given path that contains the target name.
+
+# TODO:
+# - []
+
 
 import os
 
@@ -19,22 +16,36 @@ def search_directory(path, target_name):
         target_name (string): the name of the file that user wants to search for
 
     Returns:
-        none
+        (list): a list of 2 items list, each list contains the matched filename and the path
+         of the matched file.
     """
     matched_files = []
-    with os.scandir(path) as entries:
-        for entry in entries:
-            if entry.is_file():
-                if target_name.lower() in entry.name.lower():
-                    matched_files.append([entry.name, entry.path])
-            elif entry.is_dir():
-                # This fixes the issue with recursive search
-                child_matched_files = search_directory(entry.path, target_name)
-                matched_files.extend(child_matched_files)
-    return matched_files
+    # Handle Errors if the current directory is not accessible
+    try:
+        with os.scandir(path) as entries:
+            for entry in entries:
+                # Handles Errors if there is weird entires, broken links, etc.
+                try:
+                    if entry.is_file():
+                        if target_name.lower() in entry.name.lower():
+                            matched_files.append([entry.name, entry.path])
+                    elif entry.is_dir():
+                        # This fixes the issue with recursive search
+                        child_matched_files = search_directory(entry.path, target_name)
+                        matched_files.extend(child_matched_files)
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    pass
+        return matched_files
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+    
 
 if __name__ == "__main__":
-    path = input("Enter the path to search for the keyword: ").lower()
-    target_name = input("Enter the keyword to search for: ").lower()
+    path = input("Enter the path to search for the keyword: ")
+    target_name = input("Enter the keyword to search for: ")
     result = search_directory(path, target_name)
-    print(f"Found {len(result)} matching file(s).")
+    print(f"Found {len(result)} matching file(s) with the keyword: {target_name}.")
+    for file in result:
+        print(f"MatchedFile: {file[0]} at {file[1]}")
